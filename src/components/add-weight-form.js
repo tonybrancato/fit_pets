499a6763563c677fedb3fc1be935bd4e0c059476
Tony Brancato <tonybrancato1@gmail.com>
2018-01-03 18:49:21 -0500

    changed to add-weight

--- src/components/add-weight-form.js
+++ src/components/add-weight-form.js
@@ -2,40 +2,38 @@
 
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { addWeight } from '../actions/pets'
 import Input from './input';
 import { required, nonEmpty, isTrimmed, length } from '../validators';
 
-export class AddDataForm extends React.Component {
+export class AddWeightForm extends React.Component {
   onSubmit(values) {
     const {weight} = values; 
-    const pet = {weight};
+    const petId = this.props.id;
+    const petWeight = {weight, petId};    
     return this.props
-    .dispatch(updatePet(pet)) // create action to update the pet
+    .dispatch(addWeight(petWeight, petId)) // create action to update the pet
     .then(() => this.props.dispatch);
   }
   
   render() {
     return (
       <form
-      className="add-data-form"
-      onSubmit={this.props.handleSubmit(values =>
-      	    this.onSubmit(values))}
-      onSubmit={console.log(this.props)}
-      >
+        className="add-weight-form"
+        onSubmit={this.props.handleSubmit(values =>
+          this.onSubmit(values))}>
         <div>
           <Field
             component={Input}
             type="number"
             name="weight"
-            validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3})]}
-          />
+            validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3})]}/>
         </div>
         <div>
           <button
             type="submit"
-            disabled={this.props.pristine || this.props.submitting}          
-          >Add Data
+            disabled={this.props.pristine || this.props.submitting}>
+            Add Weight
           </button>
         </div>
       </form>
@@ -44,7 +42,7 @@ export class AddDataForm extends React.Component {
 }
 
 export default reduxForm({
-  form: 'add-data',
+  form: 'add-weight',
   onSubmitFail: (errors, dispatch) =>
-    dispatch(focus('add-data', Object.keys(errors)[0])),
-  })(AddDataForm);
+    dispatch(focus('add-weight', Object.keys(errors)[0])),
+  })(AddWeightForm);
