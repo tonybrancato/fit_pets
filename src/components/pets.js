6657eaa27c644839d39dc9913dd71671f74a175c
Tony Brancato <tonybrancato1@gmail.com>
2018-01-06 20:54:17 -0500

    commit

--- src/components/pets.js
+++ src/components/pets.js
@@ -2,10 +2,14 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Accordion } from 'react-sanfona';
+import { Accordion, AccordionItem } from 'react-sanfona';
 import Pet from './pet';
+import { Line } from 'react-chartjs-2';
+import AddWeightForm from './add-weight-form';
 
 import { getPets } from '../actions/pets';
+
+import './pets.css';
 
 export class Pets extends React.Component {
   
@@ -24,8 +28,58 @@ export class Pets extends React.Component {
         <Accordion 
           // key={this.props.id}
           allowMultiple={true} 
-          className="pet-parent">         
-          {pets}
+          className="pet-parent">   
+        {this.props.pets.map(pet => {
+          return (
+      <AccordionItem
+          id={pet.id}
+          className="pet-box" 
+          expandedClassName	="pet-box-expanded" 
+          duration={200}
+          title={`${pet.name} the ${pet.species}`} >
+            <div>
+              <h3>{`${pet.name}'s Current Weight: ${pet.lastWeight} pounds`}</h3>
+              <h3>{`Age: ${pet.age}s`}</h3>
+              <div className="chart">
+                <Line 
+                  width={375}
+                  height={250}
+                  options={{
+                    maintainAspectRatio: false
+                  }}                
+                  data={{
+                    labels: pet.weightDate,
+                    datasets: [
+                      {
+                        label: `${pet.name}'s Weight History`,
+                        fill: false,
+                        backgroundColor: '#e1bee7',
+                        borderColor: '#ec97f9',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        pointBorderColor: '#863895',
+                        pointBackgroundColor: '#e1bee7',
+                        pointBorderWidth: 5,
+                        pointHoverRadius: 7,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: '#863895',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        data: pet.weight,
+                      }
+                    ]
+                  }
+                } />
+              <AddWeightForm form={`add-weight ${pet.id}`} id={pet.id}/>
+              </div>
+              
+              <div className="chart">
+              </div>
+            </div>
+        </AccordionItem>
+          );
+        })}
         </Accordion>
       </div>
     );
