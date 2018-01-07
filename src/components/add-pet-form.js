36bea2ec2320cabaa05b39272d53537f3ec35479
Tony Brancato <tonybrancato1@gmail.com>
2018-01-07 16:28:05 -0500

    testing label positioning

--- src/components/add-pet-form.js
+++ src/components/add-pet-form.js
@@ -29,8 +29,8 @@ return (
       className="add-pet-form"
       onSubmit={this.props.handleSubmit(values =>
         this.onSubmit(values))}>
-      <div className="form-section">
-        <div className="radio-section">
+      <div className=" radio-section">
+      <label className="radio-label" htmlFor="dog">Dog
           <Field
             id='dog'
             className="radio"
@@ -39,8 +39,8 @@ return (
             name="species"
             value="Dog"
           />
-          <label className="radio-label" htmlFor="dog">Dog</label>
-          <Field
+          </label>
+          <label className="radio-label" htmlFor="cat">Cat<Field
             id='cat'
             className="radio"
             component={Input}
@@ -48,10 +48,9 @@ return (
             name="species"
             value="Cat"
           />
-          <label className="radio-label" htmlFor="cat">Cat</label> 
-        </div>
+          </label> 
       </div>
-      <div className="form-section">
+      {/* <div className="form-section"> */}
         <div className="radio-section">
           <label className="radio-label" htmlFor="sex">
             <Field
@@ -70,14 +69,13 @@ return (
             />Female
           </label>
         </div>
-      </div>
+      {/* </div> */}
 
       <div className="form-section">
-        <label htmlFor="name"><h3>Pet Name</h3></label>
+      <div className='left-label'><label htmlFor="name" className='form-label'>Pet Name</label></div>
         <div>
           <Field
             component={Input}
-            placeholder="Pet Name"
             type="text"
             name="name"
             validate={[required, nonEmpty, isTrimmed]}
@@ -85,61 +83,29 @@ return (
         </div>
       </div>
       <div className="form-section">
-        <label htmlFor="birthday"><h3>Birthday</h3></label>
+      <div className='left-label'><label className='form-label' htmlFor="birthday">Birthday</label></div>
         <div>
           <Field
             type="text"
             component={renderDatePicker}
             name="birthday"
             value="0"
+            validate={[required, nonEmpty]}
           />
         </div>
       </div>
       <div className="form-section">
-        <label><h3>Weight</h3></label>
+      <div className='left-label'><label className='form-label'>Weight</label></div>
         <div>
           <Field
             type="number"
-            placeholder="equivalent to pounds"
             component={Input}
             name="weight"
-            validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3 })]}
+            validate={[required, length({ min: 1, max: 3 })]}
           />
         </div>
       </div>
-      {/* <div className="form-section checkbox-group">
-        <h3>Known Commands</h3>
-        <div className="checkbox">
-          <Field
-            type="checkbox"
-            component={Input}
-            name="command"
-            value="sit"
-            // validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3 })]}
-          />
-          <label className="checkbox-label" htmlFor="sit">Sit</label>
-        </div>
-        <div className="checkbox">
-          <Field
-            type="checkbox"
-            component={Input}
-            name="command"
-            value="stay"
-            // validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3 })]}
-          />
-          <label className="checkbox-label" htmlFor="stay">Stay</label>
-        </div>
-        <div className="checkbox">
-          <Field
-            type="checkbox"
-            component={Input}
-            name="command"
-            value="down"
-            // validate={[required, nonEmpty, isTrimmed, length({ min: 1, max: 3 })]}
-          />
-          <label className="checkbox-label" htmlFor="down">Down</label>
-        </div>
-      </div> */}
+
       <button
         type="submit"
         disabled={this.props.pristine || this.props.submitting}
