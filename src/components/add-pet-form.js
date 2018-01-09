ac1b9c67f1f935fe9f66ae997ee46c62c1f48357
Tony Brancato <tonybrancato1@gmail.com>
2018-01-08 21:24:58 -0500

    refactored cleaner code

--- src/components/add-pet-form.js
+++ src/components/add-pet-form.js
@@ -9,112 +9,117 @@ import { required, nonEmpty, isTrimmed, length } from '../validators';
 import './add-pet-form.css';
 
 export class AddPetForm extends React.Component {
-onSubmit(values) {
-const {
-  species, sex, name, birthday, weight, commands
-  } = values;
-const pet = {
-  species, sex, name, birthday, weight, commands
-  };
-return this.props
-  .dispatch(addPet(pet))
-  .then(() => this.props.dispatch);
-}
+  onSubmit(values) {
+  const {
+    species, sex, name, birthday, weight, commands
+    } = values;
+  const pet = {
+    species, sex, name, birthday, weight, commands
+    };
+  return this.props
+    .dispatch(addPet(pet))
+    .then(() => this.props.dispatch);
+  }
 
-render() {
-return (
-  <div className="add-pet-form-parent">
-    <h2 className='form-header'>Add a Pet</h2>
-    <form
-      className="add-pet-form"
-      onSubmit={this.props.handleSubmit(values =>
-        this.onSubmit(values))}>
-      <div className=" radio-section">
-      <label className="radio-label" htmlFor="dog">Dog
-          <Field
-            id='dog'
-            className="radio"
-            component={Input}
-            type="radio"
-            name="species"
-            value="Dog"
-          />
-          </label>
-          <label className="radio-label" htmlFor="cat">Cat<Field
-            id='cat'
-            className="radio"
-            component={Input}
-            type="radio"
-            name="species"
-            value="Cat"
-          />
-          </label> 
-      </div>
-      {/* <div className="form-section"> */}
-        <div className="radio-section">
-          <label className="radio-label" htmlFor="sex">
-            <Field
-              component={Input}
-              type="radio"
-              name="sex"
-              value="Male"
-            />Male
-          </label>
-          <label className="radio-label" htmlFor="sex">
-            <Field
-              component={Input}
-              type="radio"
-              name="sex"
-              value="Female"
-            />Female
-          </label>
-        </div>
-      {/* </div> */}
+  render() {
+    return (
+      <div className="add-pet-form-parent">
+        <h1 className='form-header'>Add a Pet</h1>
+        <form
+          className="add-pet-form"
+          onSubmit={this.props.handleSubmit(values =>
+            this.onSubmit(values))}>
+          <div className=" radio-section">
+            <div className='radio-input left'> 
+              <Field
+                id='dog'
+                label='Dog'
+                className="radio"
+                component={Input}
+                type="radio"
+                name="species"
+                value="Dog"
+              />
+            </div>
+            <div className='radio-input right'> 
+              <Field
+                id='cat'
+                label='Cat'
+                className="radio"
+                component={Input}
+                type="radio"
+                name="species"
+                value="Cat"
+              />
+            </div>
+          </div>
+            <div className="radio-section">
+            <div className='radio-input left'> 
+                <Field
+                  component={Input}
+                  label="Male"
+                  id="male"
+                  type="radio"
+                  name="sex"
+                  value="Male"
+                />
+              </div>
+              <div className='radio-input right'> 
+                <Field
+                  component={Input}
+                  label="Female"
+                  id="female"
+                  type="radio"
+                  name="sex"
+                  value="Female"
+                />
+              </div>
+            </div>
 
-      <div className="form-section">
-      <div className='left-label'><label htmlFor="name" className='form-label'>Pet Name</label></div>
-        <div>
-          <Field
-            component={Input}
-            type="text"
-            name="name"
-            validate={[required, nonEmpty, isTrimmed]}
-          />
-        </div>
-      </div>
-      <div className="form-section">
-      <div className='left-label'><label className='form-label' htmlFor="birthday">Birthday</label></div>
-        <div>
-          <Field
-            type="text"
-            component={renderDatePicker}
-            name="birthday"
-            value="0"
-            validate={[required, nonEmpty]}
-          />
-        </div>
-      </div>
-      <div className="form-section">
-      <div className='left-label'><label className='form-label'>Weight</label></div>
-        <div>
-          <Field
-            type="number"
-            component={Input}
-            name="weight"
-            validate={[required, length({ min: 1, max: 3 })]}
-          />
-        </div>
-      </div>
+          <div className="form-section">
+          <div className='left-label'><label htmlFor="name" className='form-label'>Pet Name</label></div>
+            <div>
+              <Field
+                component={Input}
+                type="text"
+                name="name"
+                validate={[required, nonEmpty, isTrimmed]}
+              />
+            </div>
+          </div>
+          <div className="form-section">
+          <div className='left-label'><label className='form-label' htmlFor="birthday">Birthday</label></div>
+            <div>
+              <Field
+                type="text"
+                component={renderDatePicker}
+                name="birthday"
+                value="0"
+                validate={[required, nonEmpty]}
+              />
+            </div>
+          </div>
+          <div className="form-section">
+          <div className='left-label'><label className='form-label'>Weight</label></div>
+            <div>
+              <Field
+                type="number"
+                component={Input}
+                name="weight"
+                validate={[required, length({ min: 1, max: 3 })]}
+              />
+            </div>
+          </div>
 
-      <button
-        type="submit"
-        disabled={this.props.pristine || this.props.submitting}
-      >ADD PET
-      </button>
-    </form>
-  </div>
-);
-}
+          <button
+            type="submit"
+            disabled={this.props.pristine || this.props.submitting}
+          >ADD PET
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default reduxForm({
