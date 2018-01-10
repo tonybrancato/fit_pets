c2b47aaa7ecceb9e4283e523b095575242488b12
Tony Brancato <tonybrancato1@gmail.com>
2018-01-09 19:33:18 -0500

    added no-account link

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -5,51 +5,64 @@ import { Field, reduxForm, focus } from 'redux-form';
 import Input from './input';
 import { login } from '../actions/auth';
 import { required, nonEmpty } from '../validators';
+import { Link } from 'react-router-dom';
+
+
+import './login-form.css';
+
 
 export class LoginForm extends React.Component {
-onSubmit(values) {
-return this.props.dispatch(login(values.username, values.password));
-}
+  onSubmit(values) {
+    return this.props.dispatch(login(values.username, values.password));
+  }
 
 render() {
-let error;
-if (this.props.error) {
-error = (
-  <div className="form-error" aria-live="polite">
-    {this.props.error}
-  </div>
-);
-}
-return (
-  <form
-    className="login-form"
-    onSubmit={this.props.handleSubmit(values =>
-      this.onSubmit(values),
-    )}
-  >
-    {error}
-    <Field
-      component={Input}
-      placeholder="Username"
-      type="text"
-      name="username"
-      id="usenpm strname"
-      validate={[required, nonEmpty]}
-    />
-    <Field
-      component={Input}
-      placeholder="Password"
-      type="password"
-      name="password"
-      id="password"
-      validate={[required, nonEmpty]}
-    />
-    <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
-    </button>
-  </form>
-);
-}
+  let error;
+  if (this.props.error) {
+    error = (
+      <div className="form-error" aria-live="polite">
+        {this.props.error}
+      </div>
+    );
+  }
+  return (
+    <div className='login-form-container'>
+      <form
+        className="login-form"
+        onSubmit={this.props.handleSubmit(values =>
+          this.onSubmit(values),
+        )}
+      >
+        {error}
+        <Field
+          component={Input}
+          placeholder="Username"
+          type="text"
+          name="username"
+          id="username"
+          validate={[required, nonEmpty]}
+        />
+        <Field
+          component={Input}
+          placeholder="Password"
+          type="password"
+          name="password"
+          id="password"
+          validate={[required, nonEmpty]}
+        />
+        <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
+        </button>
+      </form>
+      <div className='no-account'>
+        No Account? <Link 
+        style={{ textDecoration: 'none' }} 
+        to="/register">
+        <span>Register</span></Link> here!
+      </div>
+    </div>
+  );
+  }
 }
 
 export default reduxForm({
