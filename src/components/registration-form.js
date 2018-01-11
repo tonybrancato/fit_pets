c29f4484f36785fa11bf045488c6590f6e56caf9
Tony Brancato <tonybrancato1@gmail.com>
2018-01-11 18:53:11 -0500

    restyled registration form, matches landing page

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -6,7 +6,10 @@ import { registerUser } from '../actions/users';
 import { login } from '../actions/auth';
 import Input from './input';
 import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
+import { Link } from 'react-router-dom';
+
 import './registration-form.css';
+
 
 export class RegistrationForm extends React.Component {
 onSubmit(values) {
@@ -23,43 +26,62 @@ return this.props
 
 render() {
 return (
-  <form
-    className="login-form"
-    onSubmit={this.props.handleSubmit(values =>
-      this.onSubmit(values),
-    )}
-  >
-    <Field component={Input} type="text" name="firstName" placeholder="First Name" />
-    <Field component={Input} type="text" name="lastName" placeholder="Last Name" />
-    <Field
-      component={Input}
-      placeholder="Username"
-      type="text"
-      name="username"
-      validate={[required, nonEmpty, isTrimmed]}
-    />
-    <Field
-      component={Input}
-      placeholder="Password"
-      type="password"
-      name="password"
-      validate={[required, length({ min: 10, max: 72 }), isTrimmed]}
-    />
-    <Field
-      component={Input}
-      placeholder="Verify Password"
-      type="password"
-      name="passwordConfirm"
-      validate={[required, nonEmpty, matches('password')]}
-    />
-    <button
-      className="registration-btn"
-      type="submit"
-      disabled={this.props.pristine || this.props.submitting}
-    >
-                    Register
-    </button>
-  </form>
+  <div className='registration-form'>
+    <div className='form-container'>
+      <form
+        className="login-form"
+        onSubmit={this.props.handleSubmit(values => this.onSubmit(values)
+      )}>
+        <Field 
+          component={Input} 
+          type="text" 
+          name="firstName" 
+          placeholder="First Name" />
+
+        <Field 
+          component={Input} 
+          type="text" 
+          name="lastName" 
+          placeholder="Last Name"
+          />
+
+        <Field
+          component={Input}
+          placeholder="Username"
+          type="text"
+          name="username"
+          validate={[required, nonEmpty, isTrimmed]}
+        />
+        <Field
+          component={Input}
+          placeholder="Password"
+          type="password"
+          name="password"
+          validate={[required, length({ min: 10, max: 72 }), isTrimmed]}
+        />
+        <Field
+          component={Input}
+          placeholder="Verify Password"
+          type="password"
+          name="passwordConfirm"
+          validate={[required, nonEmpty, matches('password')]}
+        />
+        <button
+          className="registration-btn"
+          type="submit"
+          disabled={this.props.pristine || this.props.submitting}
+        >
+                        Register
+        </button>
+      </form>
+      <div className='no-account'>
+          Already Registered? <Link 
+          style={{ textDecoration: 'none' }} 
+          to="/">
+          <span>Login</span></Link> here!
+        </div>
+      </div>
+  </div>
 );
 }
 }
