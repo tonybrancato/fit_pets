8d6f03684eccb87f5e21db6a8d8207f403880faf
Tony Brancato <tonybrancato1@gmail.com>
2018-01-11 18:50:25 -0500

    Helmet added to change the page title and background

--- src/components/add-pet-page.js
+++ src/components/add-pet-page.js
@@ -3,16 +3,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
+import { Helmet } from 'react-helmet';
 import AddPetForm from './add-pet-form';
 
 export function AddPetPage(props) {
-if (!props.loggedIn) {
-return <Redirect to="/" />;
-}
+  if (!props.loggedIn) {
+    return <Redirect to="/" />;
+  }
 
 return (
   <div className="home">
+    <Helmet>
+      <title>Fit Pets - My Pets</title>     
+      <style type="text/css">
+        {` body { 
+          background-image: url("https://images.pexels.com/photos/9079/night-animal-cats-clean.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb"); 
+          background-repeat: no-repeat;
+          background-size: cover;
+        }`}
+      </style>     
+    </Helmet>
     <AddPetForm />
   </div>
 );
