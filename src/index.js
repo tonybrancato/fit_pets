3402a6b09b3c16036c79405cab729b2a256b8986
Tony Brancato <tonybrancato1@gmail.com>
2018-01-13 13:39:57 -0500

    removed commented out code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import {ConnectedRouter} from 'react-router-redux'
 import { Provider } from 'react-redux';
 import { store } from './store'
 import App from './components/app';
