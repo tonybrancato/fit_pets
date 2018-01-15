c17a75c6dec228133440bf1b655a81118c79262d
Tony Brancato <tonybrancato1@gmail.com>
2018-01-14 22:53:22 -0500

    added title to mirror Typist on landing page

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -6,6 +6,7 @@ import { Link, Redirect } from 'react-router-dom';
 
 import RegistrationForm from './registration-form';
 import { Helmet } from 'react-helmet';
+import './landing-page.css';
 
 export function RegistrationPage(props) {
 // If we are logged in (which happens automatically when registration
@@ -15,14 +16,23 @@ return <Redirect to="/dashboard" />;
 }
 return (
   <div className="home">
-    {/* <h2>Register for Pet Fit</h2> */}
+    <div className="Typist">
+      <span className="typist-header">Welcome to 
+      <span className='typist-logo'> Fit Pets</span></span>
+      <br />
+      <div className="typist-body">
+      <p>Add your pets. Update their info. Monitor their health.</p>
+      <p>Give Love</p>
+    </div>  
+  </div>
     <Helmet>
       <title>Fit Pets - Register</title>
       <style type="text/css">
-        {` body { 
+         {` body { 
             background-image: url("https://cdn.pixabay.com/photo/2015/04/10/00/47/dog-715545_1280.jpg"); 
             background-repeat: no-repeat;
             background-size: cover;
+            overflow: 'hidden';
             }`}
       </style>     
     </Helmet>
