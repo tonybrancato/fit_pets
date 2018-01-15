75a5921bfac0df27d4a737d8a827a0631e912805
Tony Brancato <tonybrancato1@gmail.com>
2018-01-14 22:45:32 -0500

    NavBar, overflow: hidden, media query

--- src/components/add-pet-page.js
+++ src/components/add-pet-page.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import AddPetForm from './add-pet-form';
+import NavBar from './navbar';
 
 export function AddPetPage(props) {
   if (!props.loggedIn) {
@@ -13,13 +14,23 @@ export function AddPetPage(props) {
 
 return (
   <div className="home">
+    <NavBar />
     <Helmet>
-      <title>Fit Pets - My Pets</title>     
+      <title>Fit Pets - Add a Pet</title>     
       <style type="text/css">
-        {` body { 
+      {`body { 
           background-image: url("https://images.pexels.com/photos/9079/night-animal-cats-clean.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb"); 
           background-repeat: no-repeat;
           background-size: cover;
+          overflow: hidden;  
+        }
+        @media only screen and (max-width: 767px) {
+          body {
+            background-image: url("https://cdn.pixabay.com/photo/2014/04/03/00/42/footprints-309158_1280.png"); 
+            background-repeat: space;
+            background-color: #000;
+            overflow: visible;          
+          }
         }`}
       </style>     
     </Helmet>
