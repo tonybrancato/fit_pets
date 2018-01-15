b6bcb83889d8dfc16847de012afb0b8b688726aa
Tony Brancato <tonybrancato1@gmail.com>
2018-01-14 22:46:32 -0500

    Helmet and NavBar

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,25 +3,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Pet from './pets';
+import { getPets } from '../actions/pets';
+import NavBar from './navbar';
 
 import './dashboard.css';
 
 export class Dashboard extends React.Component {
 
-render() {
-// Only visible to logged in users
-  if (!this.props.loggedIn) {
-    return <Redirect to="/" />;
+  render() {
+
+  // Only visible to logged in users
+    if (!this.props.loggedIn) {
+      return <Redirect to="/" />;
+    }
+  // Redirects to add-pet-form if user has no pets
+    // if (this.props.pets.length === 0) {
+    //   return <Redirect to="/addpet" />;
+    // }
+
+    return (
+      <div className="dashboard">
+        <NavBar />
+        <Helmet>
+          <title>Fit Pets - My Pets</title>        
+        </Helmet>
+        <Pet />
+      </div>
+    );
   }
-
-  return (
-
-    <div className="dashboard">
-      <Pet />
-    </div>
-  );
-}
 }
 
 const mapStateToProps = (state) => {
