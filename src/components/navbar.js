02eb2115501bb130da379be34573c363f1cb7233
Tony Brancato <tonybrancato1@gmail.com>
2018-01-16 16:18:28 -0500

    removed unused code, added PURGE STATE action

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,58 +1,55 @@
-/*eslint-disable*/
+
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { setCurrentUser, setAuthToken } from '../actions/auth';
+import { purgeState } from '../actions/pets';
 import { clearAuthToken } from '../local-storage';
-import { Navbar, Nav, NavItem, NavDropdown, NavbarBrand, MenuItem } from 'react-bootstrap';
-import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
-
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 import './navbar.css';
-import {addPet} from '../actions/pets'
-import dashboard from './dashboard'
 
 export class NavBar extends React.Component {
-logOut() {
-this.props.dispatch(setCurrentUser(null));
-this.props.dispatch(setAuthToken(null));
-clearAuthToken();
-}
-
-render() {
-  let logOutNav;
-  let addPetNav;
-  let dashboardNav;
-  if (this.props.loggedIn) {
-  logOutNav = (
-    <NavItem onClick={() => this.logOut()}>Log out</NavItem>
-  );
-  addPetNav = <LinkContainer to="/addpet"><NavItem eventKey={2}>Add Pet</NavItem></LinkContainer>;
-  dashboardNav = <IndexLinkContainer to="/"><NavItem>Home</NavItem></IndexLinkContainer>;
+  logOut() {
+    this.props.dispatch(purgeState());
+    this.props.dispatch(setCurrentUser(null));
+    this.props.dispatch(setAuthToken(null));
+    clearAuthToken();
   }
-  const navbarInstance = (
-    <Navbar 
-      fixedTop={true}
-      collapseOnSelect>
-      <Navbar.Header>
-        <LinkContainer to='/'>
-          <Navbar.Brand>
-            <span>Fit Pets</span>
-          </Navbar.Brand>
-        </LinkContainer>
-      <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-      <Nav>
-        {/* {dashboardNav} */}
-        {addPetNav}
-        {logOutNav}
-      </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-  return navbarInstance
-}
+
+  render() {
+    let logOutNav;
+    let addPetNav;
+    let dashboardNav;
+    if (this.props.loggedIn) {
+    logOutNav = (
+      <NavItem onClick={() => this.logOut()}>Log out</NavItem>
+    );
+    addPetNav = <LinkContainer to="/addpet"><NavItem eventKey={2}>Add Pet</NavItem></LinkContainer>;
+    dashboardNav = <IndexLinkContainer to="/"><NavItem>Fit Pets</NavItem></IndexLinkContainer>;
+    }
+    const navbarInstance = (
+        <Navbar 
+          fixedTop={true}
+          collapseOnSelect>
+          <Navbar.Header>
+            <LinkContainer to='/'>
+              <Navbar.Brand>
+              {dashboardNav}
+              </Navbar.Brand>
+            </LinkContainer>
+          <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+          <Nav>
+            {addPetNav}
+            {logOutNav}
+          </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    );
+    return navbarInstance
+    }
 }
 
 const mapStateToProps = state => ({
