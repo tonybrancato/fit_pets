4ba275c8d8f4ef706c2fbeb4206ee2f4f0a5eebe
Tony Brancato <tonybrancato1@gmail.com>
2018-01-16 16:21:43 -0500

    initial

---
+++ src/components/pets-header.js
@@ -0,0 +1,17 @@
+/*eslint-disable*/
+
+import React from 'react';
+import './pets-header.css';
+
+export default class PetsHeader extends React.Component {
+
+    render() {
+
+        return (
+        <div className='header-parent'>
+            <h1 className='pets-header'>My Pets</h1>
+        </div>
+        );
+    }
+}
+
