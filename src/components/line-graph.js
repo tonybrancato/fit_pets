494136c61f28d7818568db342aae106deddaccd8
Tony Brancato <tonybrancato1@gmail.com>
2018-01-18 17:27:14 -0500

    removed hardcoded width

--- src/components/line-graph.js
+++ src/components/line-graph.js
@@ -1,16 +1,18 @@
 /*eslint-disable*/
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-// export class NavBar extends React.Component
+import { defaults } from 'react-chartjs-2';
+
 export default class LineExpample extends React.Component {
   // displayName: 'LineExample',
 
   render() {
+    defaults.global.animation = false;
     return (
       // <div>
         <Line 
           data={this.props.data}
-          width={375}
+          // width={375}
           height={250}
           options={{
             maintainAspectRatio: false
