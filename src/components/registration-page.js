5cafdabfb4398cf549d5feb063ba39cd469b3971
Tony Brancato <tonybrancato1@gmail.com>
2018-01-18 17:28:06 -0500

    Redirect to addpet when logged in

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -12,49 +12,48 @@ import './registration-form.css';
 export function RegistrationPage(props) {
 // If we are logged in (which happens automatically when registration
 // is successful) redirect to the user's dashboard
-if (props.loggedIn) {
-return <Redirect to="/dashboard" />;
-}
-return (
-  <div className="home">
-    <div className="Typist">
-      <span className="typist-header">Welcome to 
-      <span className='typist-logo'> Fit Pets</span></span>
-      <br />
-      <div className="typist-body">
-      <p>Add your pets. Update their info. Monitor their health.</p>
-      <p>Give Love</p>
-    </div>  
-  </div>
-    <Helmet>
-      <title>Fit Pets - Register</title>
-      <style type="text/css">
-         {` body { 
-            min-height: 640px;
-            background-image: url(${bwdog}); 
-            background-repeat: no-repeat;
-            background-size: cover;
-            height: 100vh;
-            overflow: hidden;   
-            }
-            @media only screen and (max-width: 767px) {
-              body {
-                background-size: 1024x 768px;
-                background-position: center;
-                height: 100%;
-                overflow: visible;          
+  if (props.loggedIn) {
+    return <Redirect to="/addpet" />;
+  }
+  return (
+    <div className="home">
+      <div className="Typist">
+        <span className="typist-header">Welcome to 
+        <span className='typist-logo'> Fit Pets</span></span>
+        <br />
+        <div className="typist-body">
+          <p>Add your pets. Update their info. Monitor their health.</p>
+          <p>Give Love.</p>
+        </div>  
+      </div>
+      <Helmet>
+        <title>Fit Pets - Register</title>
+        <style type="text/css">
+          {` body { 
+              min-height: 640px;
+              background-image: url(${bwdog}); 
+              background-repeat: no-repeat;
+              background-size: cover;
+              height: 100vh;
+              overflow: hidden;   
               }
-            }`
-          }
-      </style>     
-    </Helmet>
-    <RegistrationForm />
-  </div>
-);
+              @media only screen and (max-width: 767px) {
+                body {
+                  background-size: 1024x 768px;
+                  background-position: center;
+                  height: 100%;
+                  overflow: visible;          
+                }
+            }`}
+        </style>     
+      </Helmet>
+      <RegistrationForm />
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
   loggedIn: state.auth.currentUser !== null,
-  });
+});
 
 export default connect(mapStateToProps)(RegistrationPage);
