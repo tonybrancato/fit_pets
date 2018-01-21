f604a20b26ba87ce2b4290289de4e6c6bd1eb742
Tony Brancato <tonybrancato1@gmail.com>
2018-01-21 13:15:47 -0500

    mocked action

--- src/components/pets.test.js
+++ src/components/pets.test.js
@@ -4,6 +4,18 @@ import {shallow} from 'enzyme';
 import Pet from './pets'; 
 import { store } from '../store';
 
+const mockGetPetsAction= {
+    type: 'GET_PETS'
+};
+jest.mock('../actions/pets', () => Object.assign({},
+    require.requireActual('../actions/pets'),
+    {
+        fetchBoard: jest.fn().mockImplementation(() => {
+            return mockGetPetsAction;
+        })
+    }
+));
+
 describe('<Pet />', () => {
     it('Renders without crashing', () => {
         shallow(<Pet store={store}/>);
