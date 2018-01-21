2db30b85cea407168c10b53f70740fe242baf222
Tony Brancato <tonybrancato1@gmail.com>
2018-01-21 13:14:43 -0500

    passing

--- src/components/add-weight-form.test.js
+++ src/components/add-weight-form.test.js
@@ -9,12 +9,4 @@ describe('<AddPetForm />', () => {
     it('Renders without crashing', () => {
         shallow(<AddWeightForm />);
     });
-    // it('Should dispatch addWeight action when submitted', () => {
-    //     const dispatch = jest.fn();
-    //     const wrapper = shallow(<AddWeightForm store={store} dispatch={dispatch} form='add-weight-form'/>).dive();
-    //     const value = '10';
-    //     wrapper.dive('input[type="number"]').instance().value = value;
-    //     wrapper.simulate('submit');
-    //     expect(dispatch).toHaveBeenCalledWith(addWeight(value));
-    // })
 });
